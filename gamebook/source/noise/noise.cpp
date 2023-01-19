6078b64da72df42796ab20053686627e6066e9ed
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2023-01-19 13:35:52 +0100

    Optimize unused noise state

--- gamebook/source/noise/noise.cpp
+++ gamebook/source/noise/noise.cpp
@@ -6,15 +6,16 @@ int RandomRange(int min, int max) {
 }
 
 void ApplyTextNoise(vector<string> *source, vector<char> chars, float ratio) {
-
-  for (int i = 0; i < (*source).size()-1; i++) {
-    int count = (*source)[i].length() * ratio;
-    for (int j = 0; j < (*source)[i].length(); j++) {
-      int rnd = RandomRange(0, count);
-      int rnd_char = RandomRange(1, chars.size()-1);
-      if (j < count && j % 2 == 1) {
-        if ((*source)[i][j] != ' ')
+  if (ratio > 0) {
+    for (int i = 0; i < (*source).size()-1; i++) {
+      int count = (*source)[i].length() * ratio;
+      for (int j = 0; j < (*source)[i].length(); j++) {
+        int rnd = RandomRange(0, count);
+        int rnd_char = RandomRange(1, chars.size()-1);
+        if (j < count && j % 2 == 1) {
+          if ((*source)[i][j] != ' ')
             (*source)[i][j] = chars[rnd_char];
+        }
       }
     }
   }
