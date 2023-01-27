74f9e0192ead5e142a7c015ac71d9b05ccb228f3
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2023-01-27 14:29:12 +0100

    Added char's exceptions

--- editor/src/editor.rs
+++ editor/src/editor.rs
@@ -9,7 +9,7 @@ pub trait Editor {
 pub fn edit_toml_string(ui: &mut Ui, item: &mut Item) {
     let mut content_buffer = item.as_str().unwrap().to_string().clone();
     ui.text_edit_singleline(&mut content_buffer);
-    content_buffer = filter_string(content_buffer);
+    content_buffer = filter_string(content_buffer, false);
     *item = toml_edit::value(content_buffer);
 }
 
@@ -31,11 +31,11 @@ pub fn edit_toml_string_array(ui: &mut Ui, item: &mut Item, limit: Option<usize>
     for value in array.iter_mut() {
         let mut content_buffer = value.as_str().unwrap().to_string();
         ui.text_edit_singleline(&mut content_buffer);
-        content_buffer = filter_string(content_buffer);
+        content_buffer = filter_string(content_buffer, true);
         *value = toml_edit::value(content_buffer).as_value().unwrap().clone();
     }
 
-    let mut can_edit = false;
+    let can_edit;
     match limit {
         Some(len) => {
             if array.len() < len {
@@ -58,43 +58,108 @@ pub fn edit_toml_string_array(ui: &mut Ui, item: &mut Item, limit: Option<usize>
     *item = toml_edit::value(array);
 }
 
-pub fn filter_string(input: String) -> String {
+pub fn filter_string(input: String, is_in_array: bool) -> String {
     let mut result = String::new();
 
     for i in input.chars() {
         match i {
+            ',' => {
+                if !is_in_array {
+                    result.push(',');
+                }
+            }
             '\n' => {}
-            '\"' => { result.push_str("''"); }
-            '’' => { result.push('\''); }
+            '\"' => {
+                result.push_str("''");
+            }
+            '’' => {
+                result.push('\'');
+            }
             '\r' => {}
-            'é' => {result.push('e');}
-            'É' => {result.push('E');}
-            'È' => {result.push('E');}
-            'Ë' => {result.push('E');}
-            'Ê' => {result.push('E');}
-            'è' => {result.push('e');}
-            'ë' => {result.push('e');}
-            'ê' => {result.push('e');}
-            'Ė' => {result.push('E');}
-            'ė' => {result.push('e');}
-            'ę' => {result.push('e');}
-            'Ę' => {result.push('E');}
-            'ĕ' => {result.push('e');}
-            'Ě' => {result.push('E');}
-            'ē' => {result.push('e');}
-            'Ē' => {result.push('E');}
-            'ĉ' => {result.push('c');}
-            'ç' => {result.push('c');}
-            'Ç' => {result.push('C');}
-            'Ĉ' => {result.push('C');}
-            'Á' => {result.push('A');}
-            'À' => {result.push('A');}
-            'Ã' => {result.push('A');}
-            'Â' => {result.push('A');}
-            '…' => {result.push_str("...")}
-            '»' => {result.push('\"')}
-            '«' => {result.push('\"')}
-            _ => {result.push(i); }
+            'é' => {
+                result.push('e');
+            }
+            'É' => {
+                result.push('E');
+            }
+            'È' => {
+                result.push('E');
+            }
+            'Ë' => {
+                result.push('E');
+            }
+            'Ê' => {
+                result.push('E');
+            }
+            'è' => {
+                result.push('e');
+            }
+            'ë' => {
+                result.push('e');
+            }
+            'ê' => {
+                result.push('e');
+            }
+            'Ė' => {
+                result.push('E');
+            }
+            'ė' => {
+                result.push('e');
+            }
+            'ę' => {
+                result.push('e');
+            }
+            'Ę' => {
+                result.push('E');
+            }
+            'ĕ' => {
+                result.push('e');
+            }
+            'Ě' => {
+                result.push('E');
+            }
+            'ē' => {
+                result.push('e');
+            }
+            'Ē' => {
+                result.push('E');
+            }
+            'ĉ' => {
+                result.push('c');
+            }
+            'ç' => {
+                result.push('c');
+            }
+            'Ç' => {
+                result.push('C');
+            }
+            'Ĉ' => {
+                result.push('C');
+            }
+            'Á' => {
+                result.push('A');
+            }
+            'À' => {
+                result.push('A');
+            }
+            'Ã' => {
+                result.push('A');
+            }
+            'Â' => {
+                result.push('A');
+            }
+            '…' => result.push_str("..."),
+            '»' => result.push('\"'),
+            '«' => result.push('\"'),
+            '“' => {
+                result.push_str("\'\'");
+            }
+            '”' => {
+                result.push_str("\'\'");
+            }
+            _ => {
+                result.push(i);
+            }
         }
     }
     return result;
